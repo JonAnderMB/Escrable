85f019e0335d514f4bd8e7976acc16bc93506bfa
JonAnderMB <jon_ander-1993@hotmail.com>
2012-05-10 19:14:08 +0300

    Update src/Juego/Tablero.java

--- src/Juego/Tablero.java
+++ src/Juego/Tablero.java
@@ -1,39 +1,61 @@
-// Poner valores de casillas (puntuacion)
 package Juego;
 public class Tablero {
-    
-	private static Tablero tablero = new Tablero();
-	private Casilla[][] casillas;
 	
-	private Tablero(){
-		casillas = new Casilla[14][14];
-	    { //Hacemos el tablero de 15x15
-	        for (int i=0; i < 14; i++)
-	        {
-	            for (int j=0; j < 14; j++)
-	           {
-	              casillas[i][j]=new Casilla(null,false);
-	           }
-	        }
-	    }
+		private static Tablero tablero = new Tablero();
+		private Casilla[][] casillas;
+
+		private Tablero(){
+			casillas = new Casilla[14][14];
+			{ //Hacemos el tablero de 15x15
+				for (int i=0; i < 14; i++)
+				{
+					for (int j=0; j < 14; j++)
+					{
+						casillas[i][j]= new Casilla;			//Tira error.
+					}
+				}
+			}
+		}
+
+		public static boolean comprobarCordenada(byte x, byte y){return false;}
+
+	
+    
+	private boolean comprobarJugada(Jugada jugada){
+
+		String palabra = jugada.letras.toString();
+		boolean palabraencontrada = false;
+
+			for(int x=1; x>=diccionario.length; x++){
+				for(int y=1; y>=diccionario.palabra.length;y++){
+					if (palabra == diccionario[x].palabra){
+						palabraencontrada = true;
+					}
+				}
+			}
+		return palabraencontrada; 
 	}
-	
-	public static boolean comprobarCordenada(byte x, byte y){return false;}
-    
-    public static boolean comprobarJugada(Jugada jugada)
-    {
-    	//jugada pasa coords.
-    	//NECESITO ARRAY DE FICHAS A USAR EN LA JUGADA.
-    	
-    	return false;
-    }
-   
-    public boolean realizarJugada()
-    {
-    	return false;
-    	//Se realiza lajugada
-    }
-    
+
+	private Jugada realizarJugada (Jugada jugada) {
+
+		if (comprobarJugada(jugada) == true){
+			puntuacionJugada(jugada);
+			for (int x=1; x>=jugada.letras.length; x++){
+				casillas[jugada.Xi][Jugada.Yi] = false;
+				casillas[jugada.Xi][jugada.Yi].lcas = jugada.letras[x].getLetra();
+
+				if jugada.hV == true) {
+					jugada.xI++;
+				}
+				else{
+					jugada.yI++;
+				} 
+			}
+		
+		}
+	}
+
+     
 	private boolean ponerFicha(int x, int y) 
 	{
 		if (casillas[x][y].getLibre()==false)
@@ -46,22 +68,19 @@ public class Tablero {
 		}
     }
     
-    
-	 public int puntuacionJugada(Jugada jugada)
-	    {
-			int punt=0;
-			for (int x=1; x<=jugada.getLetras().length; x++)
-			{
-				punt=punt+jugada.letras[x].getPuntos();
-			}
-			return punt;
-	    }
-	
     public void resetearTablero()
     {
     	//setTablero() = new Tablero();
     	tablero = new Tablero();
     }
     
-   
+    public int puntuacionJugada(Jugada jugada)
+    {
+		int punt=0;
+		for (int x=1; x<=jugada.getLetras().length; x++)
+		{
+			punt=punt+jugada.letras[x].getPuntos();
+		}
+		return punt;
+    }
 }
